b799dfc1d510b8a53ddc73254b0c62d5088c4706
HenryBaby <ali.dorani@gmail.com>
2026-06-12 13:38:08 +0200

    Add filename-aware OPDS download proxy

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import {
     OPDS_CATEGORY_TYPES,
     type OpdsCategory
 } from './helpers/abs.js'
-import { apiCall, loginToAudiobookshelf, proxyToAudiobookshelf } from './helpers/api.js'
+import { apiCall, downloadItemFromAudiobookshelf, loginToAudiobookshelf, proxyToAudiobookshelf } from './helpers/api.js'
 import { Library, LibraryItem } from './types/library.js'
 import { hash } from 'crypto'
 import { loadLocalizations } from './i18n/i18n.js'
@@ -181,6 +181,7 @@ declare global {
 }
 
 app.get('/opds/proxy/{*any}', (req, res) => proxyToAudiobookshelf(req, res))
+app.get('/opds/download/:itemId/:filename', (req, res) => downloadItemFromAudiobookshelf(req, res))
 
 const parseItems = (items: any): LibraryItem[] =>
     items.results
